876a76ce3e961852e652344a6367a5b4b2b5ba1c
lemon837 <freddieleman1@gmail.com>
2025-09-08 11:45:04 +0800

    Initial Commit

---
+++ Trench Assistant/Trench_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trench_AssistantApp.swift
+//  Trench Assistant
+//
+//  Created by Freddie Leman on 8/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct Trench_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
